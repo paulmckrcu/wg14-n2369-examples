525d69743111fa2a8d618dfb223087c0f9dba9a0
Paul E. McKenney <paulmck@linux.ibm.com>
2019-05-06 09:43:25 -0700

    Add alternative lifo-push algorithm from 2019 London WG14 meeting
    This is a first approximation based on memories of a verbal description.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

---
+++ lifo-push/lifo-push-london.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdatomic.h>
+#include <pthread.h>
+
+typedef char *value_t;
+
+struct node_t {
+	value_t val;
+	struct node_t *next;
+};
+
+void set_value(struct node_t *p, value_t v)
+{
+	p->val = v;
+}
+
+void foo(struct node_t *p)
+{
+	(*p->val)++;
+}
+
+// LIFO list structure
+struct node_t* _Atomic top;
+
+void list_push(value_t v)
+{
+	struct node_t *newnode = (struct node_t *) malloc(sizeof(*newnode));
+
+	set_value(newnode, v);
+	newnode->next = NULL;
+	do {
+	} while (!atomic_compare_exchange_weak(&top, &newnode->next, newnode));
+}
+
+
+void list_pop_all()
+{
+	struct node_t *p = atomic_exchange(&top, NULL);
+
+	while (p) {
+		struct node_t *next = p->next;
+
+		foo(p);
+		free(p);
+		p = next;
+	}
+}
+
+#include "fifo-stress.h"
