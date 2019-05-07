6a809e8d934622a05c1c85f197cc3577763be293
Paul E. McKenney <paulmck@linux.ibm.com>
2019-05-07 11:45:36 -0700

    lifo-push-rcu: Add variant using RCU to avoid ABA
    Although avoiding ABA is not strictly necessary from an algorithmic
    perspective, this has the side effect of avoiding accesses to dead
    pointers.  It also has the side-effect of slowing down lifo_pop_all()
    via addition of a wait for an RCU grace period, though RCU's batching
    optimizations should normally make this a latency and cache-warmth issue
    rather than a throughput issue.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

---
+++ lifo-push/lifo-push-rcu.c
@@ -0,0 +1,65 @@
+// Copyright IBM Corporation, 2019
+// Authors: Paul E. McKenney, IBM Linux Technology Center
+//	Adapted from lifo-push.c, adding RCU protection from ABA.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdatomic.h>
+#include <pthread.h>
+
+#define _GNU_SOURCE
+#define _LGPL_SOURCE
+#ifndef DO_QSBR
+#define RCU_SIGNAL
+#include <urcu.h>
+#else /* #ifndef DO_QSBR */
+#include <urcu-qsbr.h>
+#endif /* #else #ifndef DO_QSBR */
+
+typedef char *value_t;
+
+struct node_t {
+	value_t val;
+	struct node_t *next;
+};
+
+void set_value(struct node_t *p, value_t v)
+{
+	p->val = v;
+}
+
+void foo(struct node_t *p);
+
+// LIFO list structure
+struct node_t* _Atomic top;
+
+void list_push(value_t v)
+{
+	struct node_t *newnode = (struct node_t *) malloc(sizeof(*newnode));
+
+	set_value(newnode, v);
+	rcu_read_lock();
+	newnode->next = atomic_load(&top);
+	do {
+		// newnode->next may have become invalid
+	} while (!atomic_compare_exchange_weak(&top, &newnode->next, newnode));
+	rcu_read_unlock();
+}
+
+
+void list_pop_all()
+{
+	struct node_t *p = atomic_exchange(&top, NULL);
+
+	while (p) {
+		struct node_t *next = p->next;
+
+		foo(p);
+		synchronize_rcu();
+		free(p);
+		p = next;
+	}
+}
+
+#include "lifo-stress.h"
