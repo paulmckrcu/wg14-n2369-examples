fce6b1b3d85eb1b31ffd13de79282d96ef93c8f3
Paul E. McKenney <paulmck@linux.ibm.com>
2019-05-10 12:00:14 -0700

    lifo-push: Make test report errors
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- lifo-push/lifo-push-test.sh
+++ lifo-push/lifo-push-test.sh
@@ -5,11 +5,19 @@
 # Copyright IBM Corporation, 2019
 # Authors: Paul E. McKenney, IBM Linux Technology Center
 
+ret=0
 for ((i=0;i<50;i++))
 do
 	for pgm in ./lifo-push ./lifo-push-atomic ./lifo-push-atomicw ./lifo-push-int ./lifo-push-london ./lifo-push-rcu ./lifo-push-rep
 	do
 		echo Running $pgm iteration $i
-		time $pgm
+		if time $pgm
+		then
+			:
+		else
+			echo "!!! Run failed"
+			ret=1
+		fi
 	done
 done
+exit $ret
