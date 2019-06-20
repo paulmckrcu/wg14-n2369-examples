05bb7e81dd2fc1b2bfdabf7de4a29657b694b8bb
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2019-06-20 09:24:05 +0200

    replace type-safe macros with functions, add explanations

--- lifo-push/lifo-push-int-ts.c
+++ lifo-push/lifo-push-int-ts.c
@@ -31,10 +31,18 @@ struct node_t {
 	struct node_ptr_t next;
 };
 
-#define TYPE_CHECK(T, x) (0?(T){0}:(x))
-#define NODE_INT(x)	((struct node_ptr_t){ (uintptr_t)TYPE_CHECK(struct node_t *, x) })
-#define NODE_PTR(x)	((struct node_t *)(TYPE_CHECK(struct node_ptr_t, x).i))
+#define NODE_INT(x)	node_ptr2int(x)	
+#define NODE_PTR(x)	node_int2ptr(x)
 
+static inline struct node_ptr_t node_ptr2int(struct node_t *x)
+{
+	return (struct node_ptr_t){ (uintptr_t)x };
+}
+
+static inline struct node_t * node_int2ptr(struct node_ptr_t p)
+{
+	return (struct node_t *)p.i;
+}
 
 void set_value(struct node_t *p, value_t v)
 {
@@ -60,21 +68,56 @@ void list_push(value_t v)
 	newnode->next = NODE_INT(NULL);
 
 	do {
-		// The object at the address corresponding to newnode->next
-		// might be freed and reallocated at this point.  As of
-		// early May 2019, there was some disagreement among WG14
-		// committee members as to whether the C standard supports
-		// newnode->next being converted back into a pointer to
-		// a node_t structure in this case.
+		/* In 'NODE_INT(x)' a pointer is cast to 'uintptr_t'. According to
+		 * the proposal in N2362, the cast to uintptr_t in 'NODE_INT'
+		 * would expose the storage instance at 'newnode' as an invisible
+		 * side effect in the abstract machine. Consequently, all list
+		 * nodes are always exposed.
+		 *	
+		 * Then 'newnode->next' is loaded by 'atomic_compare_exchang_weak'.
+		 * If it were a pointer it may become invalid at any time if another
+		 * thread calls 'list_pop_all()' and frees the object. All accesses
+		 * to the pointer itself are then UB according to the current C
+		 * standard. We store the pointer as an integer which can still be
+		 * accessed.
+		 *
+		 * In case a new object happens to be allocated at the same place
+		 * and is added to the top of the list, the representation of
+		 * the integer (and 'struct node_ptr_t'*) for 'top' and 'next'
+		 * compare equal. In this case, 'atomic_compare_exhange_weak'
+		 * succeeds and overwrites 'top'. It points to a new object
+		 * that has would have an invalid pointer in 'next' in the
+		 * traditional pointer-based implementation. Here it has
+		 * an integer with the expected value (the abstract address
+		 * of the object).
+		 */
+
 	} while (!atomic_compare_exchange_weak(&top, &newnode->next, NODE_INT(newnode)));
 }
 
 
 void list_pop_all(void)
 {
+	/* 'top' is always valid even in a pointer-based implementation,
+	 * so nothing problematic happens here. The integer is converted
+	 * back to an pointer ('NODE_PTR') which is guarantueed to work if
+	 * 'uintptr_t' exists as a type. The rules in N2362 specify that this
+	 * works if the integer corresponds to the address of an 'exposed' and
+	 * life storage instance.
+	 * */
+
 	struct node_t *p = NODE_PTR(atomic_exchange(&top, NODE_INT(NULL)));
 
 	while (p) {
+
+		/* Here, the integer value is converted back. As the
+		 * value of the integer always corresponds to an (abstract)
+		 * address of an exposed storage instance, the conversion
+		 * yields a valid pointer. This is the case even if the
+		 * pointer it was originally derived from became invalid
+		 * because the corresponding object was freed.
+		 * */
+
 		struct node_t *next = NODE_PTR(p->next);
 
 		foo(p);
